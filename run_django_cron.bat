6c27b3b51189fad7443312d07dce07b931d79efd
natnaelabebelab <natnaelabebelab@gmail.com>
2025-11-10 15:29:48 +0300

    Stock card

--- run_django_cron.bat
+++ run_django_cron.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
-cd /d "C:\inetpub\wwwroot\scrap-management-service"
-call "C:\inetpub\wwwroot\scrap-management-service\venv\Scripts\activate.bat"
+cd /d "C:\Users\natna\Documents\Projects\JobProjects\SteelyRMIProjects\scrap-management-service-pull\scrap-management-service"
+call "C:\Users\natna\Documents\Projects\JobProjects\SteelyRMIProjects\scrap-management-service-pull\scrap-management-service\venv\Scripts\activate.bat"
 python -X utf8 manage.py runcrons >> cron_output.log 2>&1
