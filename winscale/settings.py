9f3152081dcc0aa93b95eb59d7255ab3d2f8eb38
natnaelabebelab <natnaelabebelab@gmail.com>
2025-11-26 16:30:15 +0300

    Material requisition

--- winscale/settings.py
+++ winscale/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'scrap-management-service',
-        'USER': '******',
-        'PASSWORD': '********',
+        'USER': 'postgres',
+        'PASSWORD': 'nati',
         'HOST': 'localhost',
         'PORT': '5432',
     }
